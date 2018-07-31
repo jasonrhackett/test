db31daa62f861a600c4adb7bf48daa2254f17a80
Jason Hackett <jasonhackett@jacke-3.local>
2018-07-31 17:29:00 -0500

    set up site

---
+++ main.js
@@ -0,0 +1,48 @@
+const url = 'https://us-central1-omg-codemygear.cloudfunctions.net/comments/jason';
+var content = document.getElementById('content');
+var text = document.getElementById('text');
+var email = document.getElementById('email');
+var submit = document.getElementById('submit');
+
+getComments();
+
+async function getComments() {
+  var resp = await fetch(url);
+  var comments = await resp.json();
+  var html = "";
+
+  comments.forEach(function(comment) {
+    html = html + `<br><div><span>${comment.message}</span><br>
+      <br><span><b>Posted By:</b> <a href>${comment.email}</a></span><br>
+      <br><span><b>Created at:</b> ${comment.created_at}</span><br>
+    </div>`;
+  });
+  content.innerHTML = html;
+}
+
+submit.addEventListener('click', function() {
+  postComments([{
+    "email": email.value,
+    "message": text.value
+  }]);
+});
+
+async function postComments(comment) {
+  var resp = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify(comment),
+    headers: {
+      "Content-Type": "application/json; charset=utf-8"
+    }
+  });
+  getComments();
+};
+
+// content.addEventListener('click', function(){
+//   if (event.target.className === "completeTask"){
+//     putToDo([{
+//       id: event.target.id,
+//       completed: true
+//     }]);
+//   }
+// });
